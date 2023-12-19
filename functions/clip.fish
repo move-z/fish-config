4357c8dd658086c6918b704b3b900afdde92921c
Rodolfo Boer <rodolfo.boer@previmedical.it>
2023-12-19 16:59:34 +0100

    clipboard frontend

---
+++ functions/clip.fish
@@ -0,0 +1,18 @@
+function clip -d "Copy to clipboard"
+    function _clip_wayland
+        wl-copy
+    end
+
+    function _clip_x
+        xclip -sel clipboard
+    end
+
+    if test "$XDG_SESSION_TYPE" = wayland
+        _clip_wayland
+    else if test "$XDG_SESSION_TYPE" = x11
+        _clip_x
+    else
+        echo No session!
+    end
+end
+
